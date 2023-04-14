352247625487024d557252f1e0f3a455d951b411
Nainil30 <maladkar.n@northeastern.edu>
2023-04-14 01:23:42 -0400

    addition of script files to for execution

---
+++ Create_Procedure_Functions_Script.sql
@@ -0,0 +1,12 @@
+--Script to run PLSQL Stored Procedure block for the following :
+
+--Sales_Representative_Activity :
+--	This procedure takes input from sales representative and inputs data for all parameters of a meeting
+--	This procedure checks for conversion flag and edits all tables (customer, custmer_address,customer_contact)
+
+
+--Internal_Transaction :
+
+
+--External_Transaction :
+
